57f9054fc4088527ff6107dd0a1e1893700f831a
Stephen Soltesz <stephen.soltesz@gmail.com>
2020-03-15 23:15:23 -0400

    Add controller.Setup for default handlers

--- controller/control.go
+++ controller/control.go
@@ -4,8 +4,14 @@ package controller
 
 import (
 	"context"
+	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/gorilla/handlers"
+	"github.com/justinas/alice"
 	"gopkg.in/square/go-jose.v2/jwt"
 )
 
@@ -44,4 +50,50 @@ func IsMonitoring(cl *jwt.Claims) bool {
 		return false
 	}
 	return cl.Subject == monitorSubject
+}
+
+// Setup creates sequence of access control http.Handlers. If the
+// verifier is nil then it will be excluded. If the tx controller is
+// unconfigured then it will be excluded.
+func Setup(ctx context.Context, v Verifier) (alice.Chain, *TxController) {
+	// Setup sequence of access control http.Handlers.
+	ac := alice.New()
+
+	// Controllers must be applied in specific order:
+	// 1. logging
+	// 2. access token handling
+	// 3. transmit handling - must follow tokens to identify and allow monitoring
+	ac = ac.Append(loggingHandler)
+
+	// If the verifier is not nil, include the token limit.
+	token, err := NewTokenController(v)
+	if err == nil {
+		ac = ac.Append(token.Limit)
+	}
+
+	// If the tx controller is successful, include the tx limit.
+	tx, err := NewTxController(ctx)
+	if err == nil {
+		ac = ac.Append(tx.Limit)
+	}
+
+	return ac, tx
+}
+
+func loggingHandler(next http.Handler) http.Handler {
+	return handlers.CustomLoggingHandler(os.Stderr, next, customFormat)
+}
+
+func customFormat(w io.Writer, p handlers.LogFormatterParams) {
+	// Remove the RawQuery to print less unnecessary information.
+	p.URL.RawQuery = ""
+	fmt.Fprintln(w,
+		p.Request.RemoteAddr,
+		p.TimeStamp.Format(time.RFC3339Nano),
+		p.Request.Proto,
+		p.Request.Method,
+		p.URL.String(),
+		p.StatusCode,
+		p.Size,
+	)
 }

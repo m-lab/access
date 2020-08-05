9f76b04aeb798b080bcf739f0a314203d23d7f58
Stephen Soltesz <stephen.soltesz@gmail.com>
2020-08-05 15:15:44 -0400

    Update comment

--- chanio/reader.go
+++ chanio/reader.go
@@ -7,7 +7,7 @@ func ReadOnce(r io.Reader) <-chan struct{} {
 	c := make(chan struct{})
 	go func() {
 		b := make([]byte, 1)
-		// Block on read. Will return on EOF or when client sends data (which we don't want).
+		// Block on read. Will return on EOF or when client sends data (which is discarded).
 		r.Read(b)
 		// Close channel to send reader a signal.
 		close(c)

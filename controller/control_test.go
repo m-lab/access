e4f545021760b197954b145765d3d593f718bd1e
Stephen Soltesz <stephen.soltesz@gmail.com>
2022-08-09 14:00:22 -0400

    Return TokenController from Setup

--- controller/control_test.go
+++ controller/control_test.go
@@ -73,7 +73,7 @@ func TestSetupDefault(t *testing.T) {
 			// Use synthetic proc data to allow tests to work on any platform.
 			procPath = "testdata/proc-success"
 			device = tt.device
-			ac, tx := Setup(ctx, tt.v, false, tt.hostname, Paths{"/": true})
+			ac, _, tx := Setup(ctx, tt.v, false, tt.hostname, Paths{"/": true})
 			// The tx controller only works in linux; only report errors for linux.
 			if (tx != nil) == tt.wantNil {
 				t.Errorf("Setup() tx = %v, wantNil %v", tx, tt.wantNil)
